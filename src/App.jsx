d01baabfb584a35ce1d1cc4543f96b60eb8fdb41
Binwaran <binwaran.m@gmail.com>
2025-03-25 16:18:14 +0800

    feat: App find a book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchTerm.trim()) return;
+
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+      );
+      setBooks(response.data.items || []); // เผื่อบางคำไม่มีผลลัพธ์
+    } catch (error) {
+      console.error("ดึงข้อมูลหนังสือผิดพลาด:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="พิมพ์ชื่อหนังสือ..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>{book.volumeInfo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
